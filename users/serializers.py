ebd53cd991f630a1fda1fd60e97a041371f7991d
Gustavo Gussoni <gussonigq@gmail.com>
2023-04-11 13:52:34 -0300

    fix: tarefa 1

--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,38 @@
 from rest_framework import serializers
+from users.models import User
+from rest_framework.validators import UniqueValidator
 
 
 class UserSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    email = serializers.EmailField(max_length=127, unique=True)
+    username = serializers.CharField(
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(), message="username already taken."
+            )
+        ],
+    )
+    password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(
+        max_length=127,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message="email already registered.",
+            )
+        ],
+    )
     first_name = serializers.CharField(max_length=50)
     last_name = serializers.CharField(max_length=50)
-    birthdate = serializers.DateField(allow_null=True)
+    birthdate = serializers.DateField(allow_null=True, default="")
     is_employee = serializers.BooleanField(default=False, allow_null=True)
-    is_superuser = serializers.BooleanField(default=False, allow_null=True)
+    is_superuser = serializers.BooleanField(
+        default=False, allow_null=True, read_only=True
+    )
+
+    def create(self, validated_data: dict) -> User:
+        print(validated_data["is_employee"])
+        if validated_data["is_employee"]:
+            return User.objects.create_superuser(**validated_data)
+
+        return User.objects.create_user(**validated_data)
