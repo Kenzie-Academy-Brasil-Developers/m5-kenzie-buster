17e56e2e50294af3cc554c00bd21b6fa1d62987f
pethe <pethersonreis@gmail.com>
2023-04-11 23:25:40 -0300

    feat: user's views and serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from rest_framework.validators import UniqueValidator
 
 from users.models import User
 
@@ -7,12 +7,21 @@ from users.models import User
 class UserSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
 
-    email = serializers.EmailField(max_length=127, unique=True)
+    username = serializers.CharField(
+        validators=[UniqueValidator(queryset=User.objects.all())])
+    email = serializers.EmailField(max_length=127, validators=[
+                                   UniqueValidator(queryset=User.objects.all())])
     password = serializers.CharField(write_only=True)
     first_name = serializers.CharField(max_length=50)
     last_name = serializers.CharField(max_length=50)
     birthdate = serializers.DateField(allow_null=True, default=None)
-    is_employee = serializers.BooleanField(null=True, default=False)
+    is_employee = serializers.BooleanField(allow_null=True, default=False)
+    is_superuser = serializers.BooleanField(
+        read_only=True)
 
     def create(self, validated_data: dict) -> User:
-        return User.objects.create(**validated_data)
+        print(validated_data['is_employee'])
+        if validated_data['is_employee']:
+            validated_data['is_superuser'] = True
+
+        return User.objects.create_user(**validated_data)
--- users/views.py
+++ users/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from rest_framework.views import APIView, Response, Request, status
 
-from models import User
+from .models import User
+from users.serializers import UserSerializer
 
 
 class UserView(APIView):
     @staticmethod
-    def post(request: Request):
-        user = User.objects.create_user()
+    def get(request: Request) -> Response:
+        users = User.objects.all()
+
+        serializer = UserSerializer(users, many=True)
+
+        return Response(serializer.data, status.HTTP_200_OK)
+
+    @staticmethod
+    def post(request: Request) -> Response:
+        serializer = UserSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
