59ca28a32e7dd478ab0bb03fcaf1abf073830a3c
pethe <pethersonreis@gmail.com>
2023-04-12 23:12:16 -0300

    tarefa 01: finalizada

--- users/serializers.py
+++ users/serializers.py
@@ -5,12 +5,13 @@ from users.models import User
 
 
 class UserSerializer(serializers.Serializer):
+
     id = serializers.IntegerField(read_only=True)
 
-    username = serializers.CharField(
-        validators=[UniqueValidator(queryset=User.objects.all())])
+    username = serializers.CharField(validators=[
+        UniqueValidator(queryset=User.objects.all(), message="username already taken.")])
     email = serializers.EmailField(max_length=127, validators=[
-                                   UniqueValidator(queryset=User.objects.all())])
+                                   UniqueValidator(queryset=User.objects.all(), message="email already registered.")])
     password = serializers.CharField(write_only=True)
     first_name = serializers.CharField(max_length=50)
     last_name = serializers.CharField(max_length=50)
